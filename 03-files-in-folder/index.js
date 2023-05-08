ef324dd494dd4bea9d45f983c01c70bc2e97f6ca
Georg II <118926055+Ge1o1Ge@users.noreply.github.com>
2023-05-08 15:18:24 +0200

    feat: impliment task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, "secret-folder\\")
+
+fs.readdir(filePath, (err, files) => {
+  
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  files.forEach(file => {
+    const filePath2 = path.join(filePath, file)
+    fs.stat(filePath2, (err, stats) => {
+      
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (!stats.isDirectory()) {
+        console.log(file.split(".")[0], "->" , path.extname(file).slice(1), "->", stats.size/1024, "kb");
+        return;
+      }
+    })
+  });
+})
+
+
